2d0ddcd4308eaacb7ff82747b0a6881006d3958d
Goran Stamenkovski <goran.stamenkovski@logeecom.com>
2022-06-22 11:12:43 +0200

    Release version 1.0.1
    ISSUE: CS-3322

--- src/includes/class-biller.php
+++ src/includes/class-biller.php
@@ -24,7 +24,7 @@ require_once( ABSPATH . 'wp-admin/includes/plugin.php' );
 
 class Biller {
 
-	const VERSION = '1.0.0';
+	const VERSION = '1.0.1';
 
 	const INTEGRATION_NAME = 'biller-business-invoice';
 	const BASE_API_URI = '/biller';
